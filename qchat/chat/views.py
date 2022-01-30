ebc01772fa41890c4bedb79b0cd93c31c09f7eb4
Franck Nongzanga Belemkoabga <fnbelemk@mit.edu>
2022-01-30 11:25:38 -0500

    Style change
    changed style to red and black

--- qchat/chat/views.py
+++ qchat/chat/views.py
@@ -43,3 +43,12 @@ def getMessages(request,  room):
     room_details = Room.objects.get(name=room)
     messages = Message.objects.filter(room=room_details.id)
     return JsonResponse({"messages": list(messages.values())})
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
