f6eab4c6fe5c05e53bd0c16f7aebd49aafa70af8
Franck Nongzanga Belemkoabga <fnbelemk@mit.edu>
2022-01-30 02:28:09 -0500

    chat server is functional

--- src/qchat/asgi.py
+++ src/qchat/asgi.py
@@ -1,20 +1,18 @@
-"""
-ASGI config for qchat project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
-"""
-
+# mysite/asgi.py
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qchat.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qchat.settings")
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    # Just HTTP for now. (We can add other protocols later.)
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
