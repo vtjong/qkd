17a6a15df3f516f0b20dce8be49f837094c7e020
Valene Tjong <valenetjong@Valenes-MacBook-Pro.local>
2022-01-30 09:29:58 -0500

    restructure

--- client-test.py
+++ client-test.py
@@ -1,9 +1,8 @@
-from clientserver.serverclient import send_secure_msg, receive_secure_msg
-from http import client
+from serverclient import send_secure_msg
 import sys
 sys.path.append('../')
 
 key = "Happy Hacking!"
 fullmsg = "Welcome to our MIT iQuHACK Project for QuTech."
 server_ip = "192.168.1.7"
-receive_secure_msg(key, server_ip)
+send_secure_msg(key, fullmsg, server_ip)
